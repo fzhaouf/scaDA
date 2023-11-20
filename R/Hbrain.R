4efe9de7e4a61d9e7d19397f6a311255975fc310
fzhaouf <fengdizhao@ufl.edu>
2023-11-20 15:51:52 -0500

    add example dataset

---
+++ R/Hbrain.R
@@ -0,0 +1,14 @@
+#' Human Brian dataset
+#'
+#' A dataset containing information on ...
+#'
+#' @format A data frame with X rows and Y columns:
+#' \describe{
+#'   \item{column1}{Description of column1.}
+#'   \item{column2}{Description of column2.}
+#'   \item{...}{...}
+#' }
+#' @name Hbrain
+#' @source 10X Genomics
+#' @import Seurat
+"Hbrain"
